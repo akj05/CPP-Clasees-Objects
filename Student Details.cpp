e7b016cab935f78e123f5e0dcdcec57ae7747f1b
Ankush Kumar Jha <jha.akj05@gmail.com>
2025-08-20 11:21:08 +0530

    Student Details.cpp

---
+++ Student Details.cpp
@@ -0,0 +1,44 @@
+// Ankush Jha
+// 24070123016
+
+#include<iostream>
+using namespace std;
+
+class Student {
+    public:
+    string name;
+    string branch;
+    string subject;
+    string year;
+    float result;
+};
+
+int main(){
+    Student S1;
+    S1.name="Ankuhs Jha";
+    S1.branch="E&TC";
+    S1.subject="C++";
+    S1.year="Second";
+    S1.result=7.5;
+
+    Student S2;
+    S2.name="Avi Sharma";
+    S2.branch="E&TC";
+    S2.subject="C++";
+    S2.year="Second";
+    S2.result=8.00;
+
+    Student S3;
+    S3.name="Vaishnavi Pangare";
+    S3.branch="E&TC";
+    S3.subject="C++";
+    S3.year="Second";
+    S3.result=8.50;
+
+    cout<<S1.name<<endl;
+
+    return 0;
+}
+
+// OUTPUT
+// Ankush Jha
